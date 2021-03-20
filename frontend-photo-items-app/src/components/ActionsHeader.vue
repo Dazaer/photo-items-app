732a169de5c0073b9a82a68706890332ab9e5fe4
Karolis Vaitkevicius <52960559+Dazaer@users.noreply.github.com>
2021-03-20 19:14:02 -0100

    Home page styling, add actions header component

---
+++ frontend-photo-items-app/src/components/ActionsHeader.vue
@@ -0,0 +1,29 @@
+<template>
+
+	<div class="actions-header">
+		<div class="p-2">Actions</div>
+	</div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent} from "vue";
+
+export default defineComponent({
+  name: "ActionsHeader",
+
+	setup (props, ctx) {
+    
+		return {
+		}
+
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+.actions-header {
+	@include center;
+	background-color: lavender;
+}
+</style>
--- frontend-photo-items-app/src/views/Home.vue
+++ frontend-photo-items-app/src/views/Home.vue
@@ -1,15 +1,32 @@
 <template>
-	<div class="container-fluid row">
-		<h1 class="col-md-2">Rings display</h1>
+	<div class="home-container">
+		<h1 class="title m-0 p-2" style="height: 5%">Rings display</h1>
+
+		<actions-header style="height: 5%"/>
+
+		<div class="items-container py-2" style="height: 87%">
+			<div class="items-box">
+				<div v-for="(item, index) in state.items" :key="index" class="item">
+					<img :src="item.image" alt="item image" class="item-image">
+				</div>
+			</div>
+		</div>
+
+		<footer class="footer" style="height: 3%">
+			<small>A sample test project to display rings and show their images developed by: Karolis Vaitkevicius</small>
+		</footer>
 	</div>
 </template>
 
 <script lang="ts">
 import { defineComponent, reactive } from "vue";
+import ActionsHeader from "@/components/ActionsHeader.vue";
+
 
 export default defineComponent({
 	name: "Home",
 	components: {
+		ActionsHeader,
 	},
 
 	setup () {
@@ -27,5 +44,48 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.home-container {
+	height: 100vh;
+}
 
+.title {
+	@include center;
+	font-family: Century, Candara, Arial, sans-serif;
+}
+
+.items-container {
+	background-color: #adadc8;
+}
+
+.items-box {
+	display: grid;
+	height: 100%;
+	padding: 1em;
+	overflow-y: scroll;
+	grid-template-columns: repeat(8, 1fr);
+	grid-template-rows: repeat(auto-fill, 20vh);
+	grid-column-gap: 1em;
+  grid-row-gap: 1em;
+	background-color: #adadc8;
+}
+
+.item {
+	@include center;
+	height: 20vh;
+	box-shadow: 0px 10px 10px 1px rgba(0, 0, 0, 0.78);
+	border-radius: 10px 10px 10px 10px;
+	padding: 1em;
+	border: 0px solid #000000;
+	background-color: #e6e6faba;
+}
+
+.item-image {
+	max-width: 100%;
+	max-height: 100%;
+}
+
+.footer {
+	@include center;
+	background-color: lavender;
+}
 </style>
