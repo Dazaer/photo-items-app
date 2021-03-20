f98335d56a89460ac357813c7f39963ffb9bc9cd
Karolis Vaitkevicius <52960559+Dazaer@users.noreply.github.com>
2021-03-20 01:08:44 -0100

    Added basic frontend

---
+++ frontend-photo-items-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<home />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Home from "@/views/Home.vue";
+
+export default defineComponent({
+	name: "App",
+	components: {
+		Home
+	},
+});
+</script>
+
+<style lang="scss">
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+}
+</style>
---
+++ frontend-photo-items-app/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<h1>Rings display</h1>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+
+export default defineComponent({
+	name: "Home",
+	components: {
+	},
+
+	setup () {
+
+		const state = reactive({
+		});
+
+
+		return {
+			state,
+		}
+
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
