2c2cb2521c50761b74c93bdea77c80247631813e
Karolis Vaitkevicius <52960559+Dazaer@users.noreply.github.com>
2021-03-21 01:28:55 -0100

    Add PhotosContainer component, use prop bindings

--- frontend-photo-items-app/src/components/ActionsHeader.vue
+++ frontend-photo-items-app/src/components/ActionsHeader.vue
@@ -1,7 +1,7 @@
 <template>
 
 	<div class="actions-header">
-		<div class="p-2">Amount of pictures: {{state.itemPhotos.length}}</div>
+		<div class="p-2">Amount of pictures: {{itemPhotos.length}}</div>
 
 		<div class="p-2 d-flex justify-content-center">
 
@@ -52,7 +52,7 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onMounted, reactive } from "vue";
+import { computed, defineComponent, onMounted, PropType, reactive } from "vue";
 import api from "@/utilities/api";
 import Item from "@/models/Item";
 import ItemPhoto from "@/models/ItemPhoto";
@@ -61,6 +61,13 @@ import Property from "@/models/Property";
 export default defineComponent({
 	name: "ActionsHeader",
 
+	props: {
+		itemPhotos: {
+			type: Array as PropType<Array<ItemPhoto>>,
+			required: true,
+		}
+	},
+
 	setup (props, ctx) {
 
 		const state = reactive({
@@ -68,17 +75,16 @@ export default defineComponent({
 			selectedMetal: Property.getNullSelectedProperty(),
 			selectedShape: Property.getNullSelectedProperty(),
 			items: Array<Item>(),
-      itemPhotos: Array<ItemPhoto>(),
+			itemPhotos: Array<ItemPhoto>(),
 		});
 
-    const itemPhotos = computed(() => state.itemPhotos);
+		const itemPhotos = computed(() => state.itemPhotos);
 
 		function addItem () {
 
 			console.log("added");
 
 		}
-
 
 		async function fetchItems () {
 			state.items.push(state.selectedItem);
@@ -91,19 +97,9 @@ export default defineComponent({
 
 			state.items = state.items.concat(dbItems);
 		}
-    async function fetchItemPhotos(typeId: number = 1, itemId?: number) {
-			const dbItemPhotos = await api.get(`itemphotos?typeId=${typeId}&itemId=${itemId}`);
-
-			if (!dbItemPhotos) {
-				return;
-			}
-
-			state.itemPhotos = state.itemPhotos.concat(dbItemPhotos);
-    }
 
 		onMounted(() => {
 			fetchItems();
-      fetchItemPhotos();
 		});
 
 		return {
---
+++ frontend-photo-items-app/src/components/PhotosContainer.vue
@@ -0,0 +1,68 @@
+<template>
+	<div class="photos-container">
+
+		<div v-for="(itemPhoto) in itemPhotos" :key="itemPhoto.id" class="item-photo">
+			<img :src="itemPhoto.fileName" alt="photo not available" class="image">
+		</div>
+
+	</div>
+</template>
+
+<script lang="ts">
+import ItemPhoto from "@/models/ItemPhoto";
+import { defineComponent, PropType, reactive } from "vue";
+
+export default defineComponent({
+	name: "PhotosContainer",
+
+  props: {
+		itemPhotos: {
+			type: Array as PropType<Array<ItemPhoto>>,
+			required: true,
+		}
+  },
+
+	setup (props, ctx) {
+
+		const state = reactive({
+		});
+
+
+		return {
+			state,
+		}
+
+	},
+});
+</script>
+
+<style lang="scss" scoped>
+
+.photos-container {
+	display: grid;
+	height: 100%;
+	padding: 1em;
+	overflow-y: scroll;
+	grid-template-columns: repeat(8, 1fr);
+	grid-template-rows: repeat(auto-fill, 20vh);
+	grid-column-gap: 1em;
+	grid-row-gap: 1em;
+	background-color: #adadc8;
+
+	.item-photo {
+		@include center;
+		height: 20vh;
+		box-shadow: 0px 10px 10px 1px rgba(0, 0, 0, 0.78);
+		border-radius: 10px 10px 10px 10px;
+		padding: 1em;
+		border: 0px solid #000000;
+		background-color: #e6e6faba;
+
+		.image {
+			max-width: 100%;
+			max-height: 100%;
+		}
+	}
+
+}
+</style>
